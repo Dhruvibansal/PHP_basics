178de5131de4223ea639028f787d2268152de029
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-01 02:00:30 +0530

    Add files via upload

---
+++ forloop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+	<?php
+
+ 		 for ($i=0; $i<5 ; $i++)
+   			{ 
+				echo "<p>this is the".$i."the run of the loop</p>";
+  			}
+	?>
+</body>
+</html>
