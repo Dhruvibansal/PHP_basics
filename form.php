d7edf2ebef70fde92a42e02c511f62fc5a0d4f19
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-01 02:12:00 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+	<form action = "process.php" method="POST">
+		<input type="text" name="username">
+		<input type="password" name="password">
+		<button type="submit">Login</button>
+	</form>
+	
+</body>
+</html>
