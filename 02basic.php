292fc647d280c96c7f77d9c98a8adf3b3859339f
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-01 01:45:23 +0530

    Add files via upload

---
+++ 02basic.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+//all calculations can be done in php as same as in javascript
+//echo 5+5 ;
+//echo 6>8;
+//concatinate two string in php
+echo "hello, this is string 1"."and this is string 2";
+?>
+</body>
+</html>
