bde00b4c946ca5eedcbf0d787a1df27c57f9b7a0
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-01 02:23:16 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<p>your username is:".$_POST["username"]."</p>"; // associative array here we have to put string instead of numbers
+echo "<p>your password is:".$_POST["password"]."</p>";
+
+?>
