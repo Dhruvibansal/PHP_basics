a3cfed660f29f2adfae8693a391a5f3e70d6ace3
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-01 01:34:24 +0530

    Add files via upload

---
+++ 01basic.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to php</title>
+</head>
+<body>
+	<?php
+echo "This is written in php";
+?>
+<script>
+	document.write("<p>This is written by Javascript</p>");
+</script>
+</body>
+</html>
